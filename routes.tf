44c7e12ab99482c454dab5bae59b92959d23a24f
___who1 <caranayjohn21.student@gmail.com>
2025-12-22 23:49:25 -0500

    Fixed issue on routes nat_access creation for empty prv or db

--- routes.tf
+++ routes.tf
@@ -7,13 +7,13 @@ locals {
 
   map_nat_access = merge([
     for tier, details in var.nat_access : {
-      for idx, az in(details.all ? local.map_active_azs[tier] : details.azs) :
+      for idx, az in(details.all ? try(local.map_active_azs[tier], []) : details.azs) :
       format("%s-%s", local.TIER_DICTIONARY[tier], az) => {
 
         # If AZ don't have dedicated NAT Gateway, pick one from available NAT Gateways
         nat_id = local.available_natgw[contains(local.natgw_keys, az) ? az : element(local.natgw_keys, idx)].id
         # Only create if the AZ is active(Active AZs come from the network module)
-      } if contains(local.map_active_azs[tier], az)
+      } if contains(try(local.map_active_azs[tier], []), az)
     }
   ]...)
 }
